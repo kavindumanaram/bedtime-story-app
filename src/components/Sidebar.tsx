0c997fea908b11296035418a4935a1efd51cc17e
Dimuth Kavindu Manaram <kavinduxyz@gmail.com>
2026-05-01 22:55:02 +1000

    fixed stye issue on main pages

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -40,7 +40,7 @@ export const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
       {/* Sidebar */}
       <aside
         className={`
-          fixed lg:static inset-y-0 left-0 z-50
+          fixed inset-y-0 left-0 z-50
           w-64 bg-white border-r border-gray-200
           transform transition-transform duration-300 ease-in-out
           ${isOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'}
